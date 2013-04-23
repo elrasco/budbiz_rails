249355728dbf183e8e6b62ed5772fab0041ee873
unknown <luca.rasconi@.keytechnologies>
2013-04-23 13:36:32 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Budbiz::Application.config.secret_token = '903b4593b5208eacdec62a6211678d147ef3cd5dce83aa7d4c2f7cbbf0759d4ee042421d4f9dd2f7107ef0de507ac7f288e8069a10a4d5d3ffbdacc0c37f4674'
